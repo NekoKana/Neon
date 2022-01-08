c696aeab6f6572536b93a4d6f390198eb7362c2c
swk67018 <swear1208a13@gmail.com>
2022-01-09 00:08:12 +0900

    不要なファイルの削除

--- src/functions/toMD5.js
+++
@@ -1,9 +0,0 @@
-import md5 from 'md5'
-
-export default (value) => {
-  let hashedValue = md5(value)
-  for (let i = 0; i < 50; i++) {
-    hashedValue = md5(hashedValue)
-  }
-  return hashedValue
-}
