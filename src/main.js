62fa9f437a5405e5ca2880d4bfc78964140cb086
swk67018 <swear1208a13@gmail.com>
2022-01-08 14:51:10 +0900

    vuex-storeの設定

--- src/main.js
+++ src/main.js
@@ -2,10 +2,11 @@ import { createApp } from 'vue'
 import axios from 'axios'
 import App from './App.vue'
 import router from './router'
-
+import store from './store'
 // .envからAPIのBASE_URLを読み込む
 axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL
 
 const app = createApp(App)
 app.use(router)
+app.use(store)
 app.mount('#app')
---
+++ src/store/index.js
@@ -0,0 +1,29 @@
+import { createStore } from 'vuex';
+import createPersistedState from 'vuex-persistedstate'
+
+const store = createStore({
+  state() {
+    return {
+      user: {
+        email: "",
+        password: "",
+        confirmationPassword: ""
+      }
+    }
+  },
+  mutations: {
+    setUser(state, newUser) {
+      state.user = newUser
+    }
+  },
+  plugins: [createPersistedState(
+    {
+      paths: [
+        'user'
+      ],
+      storage: window.sessionStorage
+    }
+  )]
+})
+
+export default store
