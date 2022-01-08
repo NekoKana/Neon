00a35715862c94daf8d8d72ce139e1e82a8b347f
swk67018 <swear1208a13@gmail.com>
2022-01-08 14:35:53 +0900

    axiosのBASE_URLを設定

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import { createApp } from 'vue'
+import axios from 'axios'
 import App from './App.vue'
 import router from './router'
+
+// .envからAPIのBASE_URLを読み込む
+axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL
 
 const app = createApp(App)
 app.use(router)
