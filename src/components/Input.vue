7b057874c7fe926c8ff304dc5e186aa0a160e764
swk67018 <swear1208a13@gmail.com>
2022-01-08 14:51:47 +0900

    入力用のコンポーネントを作成

---
+++ src/components/Input.vue
@@ -0,0 +1,21 @@
+<script setup>
+defineProps({
+  modelValue: {
+    type: String,
+    default: ""
+  }
+})
+
+defineEmits(['update:modelValue'])
+</script>
+
+<template>
+  <div class="control">
+    <input
+      class="input"
+      :value="modelValue"
+      @input="$emit('update:modelValue', $event.target.value)"
+      v-bind="$attrs"
+    >
+  </div>
+</template>
